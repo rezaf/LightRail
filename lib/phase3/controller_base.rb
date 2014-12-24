f252089464bbbe6584fba3fa3cfb1c97fef243f6
rezaf <reza.farajian@gmail.com>
2014-12-23 17:38:31 -0800

    initial commit

---
+++ lib/phase3/controller_base.rb
@@ -0,0 +1,17 @@
+require_relative '../phase2/controller_base'
+require 'active_support/core_ext'
+require 'erb'
+
+module Phase3
+  class ControllerBase < Phase2::ControllerBase
+    # use ERB and binding to evaluate templates
+    # pass the rendered html to render_content
+    def render(template_name)
+      contents = File.read(
+        "views/#{self.class.to_s.underscore}/#{template_name}.html.erb"
+      )
+      tbind = ERB.new(contents).result(binding)
+      render_content(tbind, "text/html")
+    end
+  end
+end
