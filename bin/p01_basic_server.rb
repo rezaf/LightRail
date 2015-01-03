f7797a38cc7a43760c06c00f20b286dbc4cee6c2
rezaf <reza.farajian@gmail.com>
2015-01-02 18:31:16 -0800

    phase I update

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,11 +1,6 @@
 require 'webrick'
 
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
-
-server = WEBrick::HTTPServer.new(:Port => 3000)
+server = WEBrick::HTTPServer.new(Port: 3000)
 
 server.mount_proc("/") do |request, response|
   response.content_type = "text/text"
@@ -13,7 +8,7 @@ server.mount_proc("/") do |request, response|
 end
 
 trap('INT') do
-    server.shutdown
+  server.shutdown
 end
 
 server.start
