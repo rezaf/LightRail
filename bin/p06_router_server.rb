759fe4cd498df010ecc145c6dd3a24eae16bfa4c
rezaf <reza.farajian@gmail.com>
2015-01-04 19:12:23 -0800

    phase VI

--- bin/p06_router_server.rb
+++ bin/p06_router_server.rb
@@ -2,21 +2,15 @@ require 'webrick'
 require_relative '../lib/phase6/controller_base'
 require_relative '../lib/phase6/router'
 
-
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPRequest.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/HTTPResponse.html
-# http://www.ruby-doc.org/stdlib-2.0/libdoc/webrick/rdoc/WEBrick/Cookie.html
-
 $cats = [
-  { id: 1, name: "Curie" },
-  { id: 2, name: "Markov" }
+  { id: 1, name: "Nickie" },
+  { id: 2, name: "Marie" }
 ]
 
 $statuses = [
-  { id: 1, cat_id: 1, text: "Curie loves string!" },
-  { id: 2, cat_id: 2, text: "Markov is mighty!" },
-  { id: 3, cat_id: 1, text: "Curie is cool!" }
+  { id: 1, cat_id: 1, text: "Nickie loves string!" },
+  { id: 2, cat_id: 2, text: "Marie is mighty!" },
+  { id: 3, cat_id: 1, text: "Nickie is cool!" }
 ]
 
 class StatusesController < Phase6::ControllerBase
@@ -24,7 +18,6 @@ class StatusesController < Phase6::ControllerBase
     statuses = $statuses.select do |s|
       s[:cat_id] == Integer(params[:cat_id])
     end
-
     render_content(statuses.to_s, "text/text")
   end
 end
@@ -42,9 +35,8 @@ router.draw do
 end
 
 server = WEBrick::HTTPServer.new(Port: 3000)
-server.mount_proc('/') do |req, res|
-  route = router.run(req, res)
+server.mount_proc('/') do |request, response|
+  route = router.run(request, response)
 end
-
 trap('INT') { server.shutdown }
 server.start
